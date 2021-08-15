0e4b0e4a988cbe74be149dab5c142eb351c16bc5
William Sørensen <william@resight.no>
2021-08-15 09:24:24 +0200

    fix: extract comments

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
 #![feature(core_intrinsics)]
 #![feature(vec_into_raw_parts)]
 mod binary_tree;
+mod header;
 mod huff_decoder;
 mod huff_encoder;
 mod types;
 mod utils;
-mod header;
 
 pub use crate::huff_encoder::*;
 use std::env;
 use std::fs;
-
-// Header Layout:
-// Vers offB     Symbols-Size     Content size (max 4.294967296gb)
-// 0000 0000 00000000000000000000 00000000000000000000000000000000
-// sizes = 8, 16, 32, 64
-
-// Tree symbols
-// 00 = end node
-// 01 = node followed by leaf
-// 10 = node followed by node
-// 11 = node followed by 2 leafs
 
 fn main() -> std::io::Result<()> {
     // let args: Vec<String> = std::env::args().collect();
